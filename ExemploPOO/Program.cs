359715cf80c2828a593ada349bd5bf7d3446c009
PedroLeao85 <phalves1@hotmail.com>
2022-02-22 23:09:49 -0300

    Cria classe Retangulo

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -7,11 +7,24 @@ namespace ExemploPOO
     {
         static void Main(string [] args)
         {
-            Pessoa p1 = new Pessoa();
-            p1.Nome = "Pedro";
-            p1.Idade = 37;
+           //Valores Validos
+            Retangulo r1 = new Retangulo();
+            r1.DefinirMedidas(30 , 30);
+            System.Console.WriteLine($"Área:{r1.ObterArea()}");
+            
+            //Valores Invalidos
+            Retangulo r2 = new Retangulo();
+            r2.DefinirMedidas(0 , 0);
+            System.Console.WriteLine($"Área:{r2.ObterArea()}");
+            
+            
+            
+            
+            // Pessoa p1 = new Pessoa();
+            // p1.Nome = "Pedro";
+            // p1.Idade = 37;
 
-            p1.Apresentar();
+            // p1.Apresentar();
         }
     }
 }
